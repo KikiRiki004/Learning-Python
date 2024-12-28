cb671e6e1659a9b0daaf35c4a080212219291e33
KikiRiki004 <krivonosovk.dev@gmail.com>
2024-12-27 20:16:58 -0400

    Doubly

---
+++ Linked_lists/doubly_linked_list.py
@@ -0,0 +1,9 @@
+# 1. The previous field references the previous node.
+# 2. The next field references the next node.
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.previous = None
+
